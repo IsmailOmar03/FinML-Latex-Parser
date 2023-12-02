116e264831388c8df41aa04bace6458b21fd7a05
IsmailOmar03 <102528799+IsmailOmar03@users.noreply.github.com>
2023-12-02 12:52:42 -0500

    Update main.py
    Made user input csv files via command line arguments rather than be hardcoded

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 #make necessary imports to handle data
+import sys
+
 import pandas as pd
 import numpy as np
 
@@ -11,14 +13,17 @@ def print_csv(df, var):
         if (df.iloc[i][f'{sig_lvls[j]}_percentile_{var}'] * df.iloc[i][f'{100 - sig_lvls[j]}_percentile_{var}']) > 0:
             s_count = 3 - j
             break
-    f.write(f' ${df.iloc[i][f"lambda_{var}"]:.2e}^' + '{' + f'{"*" * (s_count)}' + '}$ ')
+    f.write(f' ${df.iloc[i][f"lambda_{var}"]:.2e}^' + '{' + f'{"*" * s_count}' + '}$ ')
     f.write(f' & ({df.iloc[i][f"st_error_{var}"]:.2e}) ')
 
 #main function
 if __name__ == '__main__':
+    if len(sys.argv) < 2:
+        print("Provide command line args. First one for Two-pass csv, second for Five-step")
+        exit()
     #read in data from csv file into dataframe
-    df_tp = pd.read_csv("./data/lambda_G_estimates_CISE.csv")
-    df_fs = pd.read_csv("./data/lambda_G_estimates_CISE_td.csv")
+    df_tp = pd.read_csv(sys.argv[1])
+    df_fs = pd.read_csv(sys.argv[2])
     #header for the tex file
     doc_header = r"""\begin{document}
 \begin{tabular}{lcccccccc}
